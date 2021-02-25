5edbd25e1b469ffaeea2cabdfd560ad85792efeb
Ederam <53803895+Ederam@users.noreply.github.com>
2021-02-25 16:47:09 -0500

    Se agregan los componentes principales para iniciar la aplicacion

--- StoreNG/OnLineStoreNG/src/app/app.module.ts
+++ StoreNG/OnLineStoreNG/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './public/home/default/default.component';
+import { SideMenuComponent } from './public/master-page/side-menu/side-menu.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    SideMenuComponent,
+    HeroComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
