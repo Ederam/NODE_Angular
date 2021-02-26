46982cc72338cb70e8b534f2efae91bb35d556cc
Ederam <53803895+Ederam@users.noreply.github.com>
2021-02-26 08:52:10 -0500

    se realiza configuracion de ruteo de archivos por el path

--- StoreNG/OnLineStoreNG/src/app/app-routing.module.ts
+++ StoreNG/OnLineStoreNG/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /**Configuracion de rutas */
+  
+ {
+    path: 'home',
+    component: DefaultComponent
+  }, 
+  {
+    path: '',
+    pathMatch:'full',
+    redirectTo: '/home'
+  }, 
+
+  /**Este debe ser el ultimo */
+ {
+    path: '**',
+    redirectTo: '/home'
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
